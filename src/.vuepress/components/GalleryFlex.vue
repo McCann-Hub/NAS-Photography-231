895e8e53c6ad973c8166c45f46d9296cbc5f8647
Jimmy McCann <jabez.m.007@gmail.com>
2021-01-18 15:55:15 -0600

    gallery flex

---
+++ src/.vuepress/components/GalleryFlex.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="gallery" class="flex flex-wrap justify-center items-center">
+    <img
+      v-for="image in images"
+      :key="image.name"
+      :alt="image.name"
+      :src="image.src"
+      class="rounded m-4 sm:w-full md:w-1/2 lg:w-1/3 xl:w-1/5 2xl:w-1/7"
+    />
+  </div>
+</template>
+
+<script>
+import Gallery from "@mixins/gallery.js";
+
+export default {
+  name: "GalleryFlex",
+  mixins: [Gallery],
+};
+</script>
